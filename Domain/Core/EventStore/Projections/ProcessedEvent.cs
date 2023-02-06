7e72f2cb7bf652b06eaea8ed60cb8dfc31c64311
Luka Ogadze <lukaogadze@live.com>
2023-02-06 17:38:45 +0400

    fixed bug

--- Domain/Core/EventStore/Projections/ProcessedEvent.cs
+++ Domain/Core/EventStore/Projections/ProcessedEvent.cs
@@ -4,12 +4,12 @@ public class ProcessedEvent
 {
     public Guid Id { get; private set; }
     public Guid AggregateId { get; private set; }
-    public Guid StoredEventId { get; private set; }
+    public ulong StoredEventId { get; private set; }
     public DateTimeOffset CreateDate { get; private set; }
     
     private ProcessedEvent() {}
     
-    public ProcessedEvent(Guid id, Guid aggregateId, Guid storedEventId, DateTimeOffset createDate)
+    public ProcessedEvent(Guid id, Guid aggregateId, ulong storedEventId, DateTimeOffset createDate)
     {
         Id = id;
         AggregateId = aggregateId;
